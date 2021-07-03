57751ef637709bbc569fc76b7a90024c4d25341b
Pitsanu Kittipittayakorn <crack.kiddie@gmail.com>
2021-07-04 00:36:06 +0700

    Change translate instruction from factory

--- projects/06/assembler/src/main/kotlin/main.kt
+++ projects/06/assembler/src/main/kotlin/main.kt
@@ -1,5 +1,6 @@
 import com.github.ajalt.clikt.core.CliktCommand
 import com.github.ajalt.clikt.parameters.arguments.argument
+import instructions.HackInstruction
 import java.io.File
 
 fun main(args: Array<String>) = Assembler().main(args)
@@ -8,49 +9,11 @@ class Assembler: CliktCommand(help = "Translate FILE Hack assembly to binary.") 
 
     private val file by argument()
 
-    private val aPattern = "^@(.*)$".toRegex()
-    private val cDest = "([AMD]|[AM]D|AM|AMD)"
-    private val cComp = "([01]|-[1AMD]|!?[AMD]|[AMD][+-]1|D[-+&|][AM]|[AM]-D)?"
-    private val cJump = "(JGT|JEQ|JGE|JLT|JNE|JLE|JMP)"
-    private val cPattern = "^(?>$cDest=)?$cComp?$(?>$cJump;)?".toRegex()
-
     override fun run() {
         File(file).readLines()
             .map { CleanUp(it).removeComments() }
             .filter { it.isNotEmpty() }
-            .map { handlingInstructions(it) }
+            .map { HackInstruction.matchWith(it).translate() }
             .forEach { echo(it) }
-    }
-
-    private fun handlingInstructions(line: String): String {
-        return when {
-            aPattern.matches(line) -> translateInstructionA(line)
-            cPattern.matches(line) -> translateInstructionC(line)
-            else -> line
-        }
-    }
-
-    private fun translateInstructionA(line: String): String  {
-        aPattern.matchEntire(line)?.groupValues?.toMutableList()?.let { values ->
-            values.removeFirstOrNull()
-            values
-                .filter { it.isNotEmpty() }
-                .forEach {
-                    echo("      $it")
-                }
-        }
-        return "InstructionA $line"
-    }
-
-    private fun translateInstructionC(line: String): String  {
-        cPattern.matchEntire(line)?.groupValues?.toMutableList()?.let { values ->
-            values.removeFirstOrNull()
-            values
-                .filter { it.isNotEmpty() }
-                .forEach {
-                    echo("      $it")
-                }
-        }
-        return "InstructionC $line"
     }
 }
