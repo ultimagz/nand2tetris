848b6434cb1d1fa8d8ab6af99a9c8f7d9538ee75
Pitsanu Kittipittayakorn <crack.kiddie@gmail.com>
2021-07-04 00:34:20 +0700

    Add Instruction Pattern class

---
+++ projects/06/assembler/src/main/kotlin/instructions/InstructionPattern.kt
@@ -0,0 +1,16 @@
+package instructions
+
+enum class InstructionPattern(internal val pattern: String) {
+    C_DEST("([AMD]|[AM]D|AM|AMD)"),
+    C_COMP("([01]|-[1AMD]|!?[AMD]|[AMD][+-]1|D[-+&|][AM]|[AM]-D)?"),
+    C_JUMP("(JGT|JEQ|JGE|JLT|JNE|JLE|JMP)"),
+    C_DEST_COMP("^(?>${C_DEST.pattern}=)${C_COMP.pattern}$"),
+    C_DEST_JUMP("^${C_DEST.pattern}(?>;${C_JUMP.pattern})$"),
+    C_COMP_JUMP("^${C_COMP.pattern}(?>;${C_JUMP.pattern})$"),
+    C("^(?>${C_DEST.pattern}=)?${C_COMP.pattern}?(?>;${C_JUMP.pattern})?$"),
+    A("^@(.*)$");
+
+    fun matches(code: String): Boolean = pattern.toRegex().matches(code)
+
+    fun matchEntire(code: String): MatchResult? = pattern.toRegex().matchEntire(code)
+}
