616619c3d17543410fb3992d22ee739224c3dcac
Pitsanu Kittipittayakorn <crack.kiddie@gmail.com>
2021-07-04 02:44:06 +0700

    Save output to .hack file same as input directory

--- projects/06/assembler/src/main/kotlin/main.kt
+++ projects/06/assembler/src/main/kotlin/main.kt
@@ -8,13 +8,45 @@ fun main(args: Array<String>) = Assembler().main(args)
 class Assembler: CliktCommand(help = "Translate FILE Hack assembly to binary.") {
 
     private val file by argument()
+    private val customOutPath: String? = null
 
     override fun run() {
         File(file).readLines()
+            .asSequence()
             .map { CleanUp(it).removeComments() }
             .filter { it.isNotEmpty() }
             .map { echo(it); it }
             .map { HackInstruction.matchWith(it).translate() }
-            .forEach { echo(it) }
+            .toList()
+            .run {
+                val outFile = createOutputFile(customOutPath)
+                saveToHackFile(outFile, this)
+            }
+    }
+
+    private fun saveToHackFile(file: File, data: List<String>) {
+        if (!file.exists()) {
+            file.createNewFile()
+        }
+
+        file.printWriter().use { writer ->
+            data.forEach {
+                writer.println(it)
+            }
+        }
+    }
+
+    private fun createOutputFile(customOutPath: String?): File {
+        val outPath = if (customOutPath.isNullOrEmpty()) {
+            val parentDir = File(file).parent
+            val fileName = File(file).nameWithoutExtension
+            val fileExtension = ".hack"
+
+            "$parentDir/$fileName$fileExtension"
+        } else {
+            customOutPath
+        }
+
+        return File(outPath)
     }
 }
