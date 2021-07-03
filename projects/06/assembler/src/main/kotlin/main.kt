dae00c71670582c1b927ce382435a6fd74c54b6f
Pitsanu Kittipittayakorn <crack.kiddie@gmail.com>
2021-07-03 23:18:48 +0700

    Separate instruction A and C

--- projects/06/assembler/src/main/kotlin/main.kt
+++ projects/06/assembler/src/main/kotlin/main.kt
@@ -8,13 +8,49 @@ class Assembler: CliktCommand(help = "Translate FILE Hack assembly to binary.") 
 
     private val file by argument()
 
+    private val aPattern = "^@(.*)$".toRegex()
+    private val cDest = "([AMD]|[AM]D|AM|AMD)"
+    private val cComp = "([01]|-[1AMD]|!?[AMD]|[AMD][+-]1|D[-+&|][AM]|[AM]-D)?"
+    private val cJump = "(JGT|JEQ|JGE|JLT|JNE|JLE|JMP)"
+    private val cPattern = "^(?>$cDest=)?$cComp?$(?>$cJump;)?".toRegex()
+
     override fun run() {
         File(file).readLines()
             .map { CleanUp(it).removeComments() }
+            .filter { it.isNotEmpty() }
+            .map { handlingInstructions(it) }
             .forEach { echo(it) }
     }
 
-    internal fun removeComments(line: String): String {
-        return line.replace(regex = Regex("//.*"), replacement = "").trim()
+    private fun handlingInstructions(line: String): String {
+        return when {
+            aPattern.matches(line) -> translateInstructionA(line)
+            cPattern.matches(line) -> translateInstructionC(line)
+            else -> line
+        }
+    }
+
+    private fun translateInstructionA(line: String): String  {
+        aPattern.matchEntire(line)?.groupValues?.toMutableList()?.let { values ->
+            values.removeFirstOrNull()
+            values
+                .filter { it.isNotEmpty() }
+                .forEach {
+                    echo("      $it")
+                }
+        }
+        return "InstructionA $line"
+    }
+
+    private fun translateInstructionC(line: String): String  {
+        cPattern.matchEntire(line)?.groupValues?.toMutableList()?.let { values ->
+            values.removeFirstOrNull()
+            values
+                .filter { it.isNotEmpty() }
+                .forEach {
+                    echo("      $it")
+                }
+        }
+        return "InstructionC $line"
     }
 }
