2aebe510897fa2db81ba5f7dc63c0b0a19e91012
Pitsanu Kittipittayakorn <crack.kiddie@gmail.com>
2021-07-03 22:57:20 +0700

    Clean up comment

--- projects/06/assembler/src/main/kotlin/main.kt
+++ projects/06/assembler/src/main/kotlin/main.kt
@@ -1,3 +1,20 @@
-fun main(args: Array<String>) {
-    println("Hello World!")
+import com.github.ajalt.clikt.core.CliktCommand
+import com.github.ajalt.clikt.parameters.arguments.argument
+import java.io.File
+
+fun main(args: Array<String>) = Assembler().main(args)
+
+class Assembler: CliktCommand(help = "Translate FILE Hack assembly to binary.") {
+
+    private val file by argument()
+
+    override fun run() {
+        File(file).readLines()
+            .map { removeComments(it) }
+            .forEach { echo(it) }
+    }
+
+    internal fun removeComments(line: String): String {
+        return line.replace(regex = Regex("//.*"), replacement = "").trim()
+    }
 }
